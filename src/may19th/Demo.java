e685e9408a9176759e682db4769b0930831e6013
automationcatalogue <automation.catalogue@gmail.com>
2021-07-07 08:49:56 +0530

    Some code changes in java programs

--- src/may19th/Demo.java
+++ src/may19th/Demo.java
@@ -6,11 +6,14 @@ public class Demo {
 	void print(int a, int b){
 		int c=a+b;
 		System.out.println("Addition of two number is :"+c);
+		int d=a-b;
+		System.out.println("Subtraction of two numbers is :"+d);
 	}
 	
 	
 	void show(){
 		System.out.println("Core Java...!!!");
+		System.out.println("Advanced Java...!!!");
 	}
 	public static void main(String[] args) {
 		Demo ob = new Demo();
---
+++ src/misc/Welcome.java
@@ -0,0 +1,7 @@
+package misc;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java programming..!!!");
+	}
+}
