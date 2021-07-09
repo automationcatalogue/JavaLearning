be4d9518115ed569f1c48decf849195658bfebc5
vijgodbole <vijethagodbole.2012@gmail.com>
2021-07-09 21:53:08 +0530

    Modification done on purpose

--- src/may2nd/HelloWorld.java
+++ src/may2nd/HelloWorld.java
@@ -16,9 +16,9 @@ public class HelloWorld {
 		System.out.println("Value of ob1.x is: "+ob1.x+" Value of ob1.y is :"+ob1.y);//100 70
 		System.out.println("Value of ob1.a is: "+ob1.a+" Value of ob1.b is :"+ob1.b); //100 70
 		
-		HelloWorld ob2 = new HelloWorld();
-		System.out.println("Value of ob2.x is: "+ob2.x+" Value of ob2.y is :"+ob2.y);//90 80
-		System.out.println("Value of ob2.a is: "+ob2.a+" Value of ob2.b is :"+ob2.b);//100 70
+		HelloWorld object2 = new HelloWorld();
+		System.out.println("Value of object2.x is: "+object2.x+" Value of ob2.y is :"+object2.y);//90 80
+		System.out.println("Value of object2.a is: "+object2.a+" Value of ob2.b is :"+object2.b);//100 70
 		
 		
 	}
--- src/may5th/Demo.java
+++ src/may5th/Demo.java
@@ -2,22 +2,22 @@ package may5th;
 
 public class Demo {
 	public static void main(String[] args) {
-		Demo ob = new Demo();
-		ob.addNumbers(10,20);//method call statement
+		Demo obiect = new Demo();
+		obiect.addNumbers(10,20);//method call statement
 		
-		ob.addNumbers(30, 50);
+		obiect.addNumbers(30, 50);
 		
-		ob.addNumbers(100,45);
+		obiect.addNumbers(100,45);
 	}
 	
 	void addNumbers(int a, int b){//Instance method
 		int c= a+b;
 		System.out.println("Addition of a,b is :"+c);
-		
-		getMax(a, b);//method call statement
+		//method call statement
+		getMax(a, b);
 	}
 	
-	void getMax(int x, int y){//x=b==> 20, 50
+	void getMax(int x, int y){///x=b==> 20, 50
 		if(x>y){
 			System.out.println("X is greater value :"+x);
 		}else if(x<y){
