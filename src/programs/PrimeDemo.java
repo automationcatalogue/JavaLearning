9dd4ae863c45d7bf30870f06b0da7b60fb69edae
vijgodbole <vijethagodbole.2012@gmail.com>
2021-07-09 08:44:10 +0530

    Changes made to prime demo code

--- src/programs/PrimeDemo.java
+++ src/programs/PrimeDemo.java
@@ -2,19 +2,22 @@ package programs;
 
 public class PrimeDemo {
 	public static void main(String[] args) {
-		int i=1, n=13;
+		int k=1, m=13;
 		int count=0;
+		System.out.println("About to execute prime number logic");
 		do{
-			if(n%i==0){
+			if(m%k==0){
 				count++;
+				System.out.println("If conition is being checked");
 			}
-			++i;
-		}while(i<=n);
+			++k;
+		}while(k<=m);
+		System.out.println("Prim  number code is completed");
 		
 		if(count==2){
 			System.out.println("It is a Prime number");
 		}else{
-			System.out.println("It is not a prime number, because "+n+" is divisible by "+count+" times");
+			System.out.println("It is not a prime number, because "+m+" is divisible by "+count+" times");
 		}
 	}
 }
