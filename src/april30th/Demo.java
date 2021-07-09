52b644fe4cdcab581824fb40d6af89c8f6bd7267
vijgodbole <vijethagodbole.2012@gmail.com>
2021-07-09 08:18:36 +0530

    Minute changes to code

--- src/april30th/Demo.java
+++ src/april30th/Demo.java
@@ -1,21 +1,19 @@
 package april30th;
 
 public class Demo {
-	
-	
-	
+		
 	public static void main(String[] args) {
-		 int a=10, b=20;//Local variable
+		 int a=10, b=20;
 		int c=a+b;
 		System.out.println("Adition of a,b is :"+c);
 	}
 	
 	void getData(){
 		int g=34, h=45;
-		int m=g+h;
-		System.out.println("Addition of g,h is :"+m);
+		int I=g+h;
+		System.out.println("Addition of g,h is :"+I);
 	}
 	
-	static int x=100, y=200; //Class variables
+	static int x=100, y=200; 
 }
 
--- src/misc/Welcome.java
+++
@@ -1,7 +0,0 @@
-package misc;
-
-public class Welcome {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java programming..!!!");
-	}
-}
